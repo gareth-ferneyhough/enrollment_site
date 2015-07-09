0cdf475f323572d8836366b73f9c8d182fa15932
Gareth Ferneyhough <gareth.ferneyhough@gmail.com>
2015-07-09 12:40:48 -0700

    Added stylesheet and prettyfied layout.

--- enrollment.js
+++ enrollment.js
@@ -1,32 +1,28 @@
- //  $("input[type='text']").on("click", function () {
- //   $(this).select();
- // });
+$("input[type='text']").on("click", function () {
+ $(this).select();
+});
 
-  $("input").click(function(e){
-    e.select();
-  });
+$("#search-subject-form").submit(function(e) {
+  var formURL = e.currentTarget.action;
+  var formMethod = e.currentTarget.method;
+  debugger;
 
-  $("#searchSubjectForm").submit(function(e) {
-    var formURL = e.currentTarget.action;
-    var formMethod = e.currentTarget.method;
-    debugger;
-
-    $.ajax({
-      type: formMethod,
-      url: formURL,
-      data: $("#searchSubjectForm").serialize(),
-      success: function (response) {
+  $.ajax({
+    type: formMethod,
+    url: formURL,
+    data: $("#search-subject-form").serialize(),
+    success: function (response) {
         // Clear all existing rows in the result table
-        $("#subject_table > tbody > tr").remove();
+        $("#project_table > tbody > tr").remove();
 
         var trHTML = '';
         $.each(JSON.parse(response), function (i, item) {              
           trHTML += '<tr><td>' + item.projectName + '</td><td>' +
-            item.status + '</td><td>' + 
-            item.eligibility + ' (' + item.secondary + ')</td><td>' + 
-            item.startDate + '</td></tr>';
+          item.status + '</td><td>' + 
+          item.eligibility + ' (' + item.secondary + ')</td><td>' + 
+          item.startDate + '</td></tr>';
         });
-        $('#subject_table > tbody').append(trHTML);
+        $('#project_table > tbody').append(trHTML);
       },
       error: function(request, status, error)
       {
